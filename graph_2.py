c836bd3ff4c6d29117b702a4f259db043d2241ef
alberto castillo gordón <acastillogordon@gmail.com>
2017-12-13 01:14:51 -0500

    Refactor Graph_2

--- graph_2.py
+++ graph_2.py
@@ -4,15 +4,18 @@
 
 def main():
     word = str(input("Ingrese una palabra: "))
-    paths = {'q0': lambda letter: 'q0' if letter == 'a' else 'q1',
-            'q1': lambda letter: 'q1' if letter == 'a' else 'q2',
+    paths = {'q0': lambda letter: 'q1' if letter == 'a' else 'q4',
+            'q1': lambda letter: 'Error' if letter == 'a' else 'q2',
             'q2': lambda letter: 'q2' if letter == 'a' else 'q3',
-            'q3': lambda letter: 'q3' if letter == 'a' else 'Error'}
-    s = 'q0' # Estado Actual
+            'q3': lambda letter: 'q3' if letter == 'a' else 'Error',
+            'q4': lambda letter: 'q4' if letter == 'a' else 'q5',
+            'q5': lambda letter: 'q5' if letter == 'a' else 'Error'
+            }
+    s = 'q0' #Estado actual
 
     for letter in word:
         if letter not in ['a', 'b'] or s == 'Error':
-            print("No pertenece al diccionario")
+            print("No pertenece al diccionartio")
             break
         else:
             s = paths[s](letter)
